8753c128eaaa32e777ef871ea0bf747b501a082a
Maharshi <maharshi365@gmail.com>
2019-01-30 13:22:23 -0500

    General Frontend Update
    Finished About us page. Changed the way layout works so that all pages extend it now.

--- HMI/Website/__init__.py
+++ HMI/Website/__init__.py
@@ -41,7 +41,7 @@ def createApp():
 
     @app.route('/about')
     def about():
-        return render_template('about.html', title="About Us", caption="Learn More About Us!")
+        return render_template('about.html', title="About Us", caption="Learn More About Us!", teammates=teammates)
 
     @app.route('/track')
     def track():
@@ -53,6 +53,44 @@ def createApp():
 
     return app
 
+
+teammates = [
+    {
+        'Name': 'Maharshi Patel',
+        'Bio': 'Maharshi is a Electrical B.Eng candidate at McMaster University. In this project he developed the dispension system and the HMI. He also worked on group administration. He hopes that his contributions in this project will lead to new career oportunities.',
+        'Task1': 'Drink Dispension System',
+        'Task2': 'Human Machine Interface',
+        'Task3': 'Group Administration',
+        'LinkedIn': 'https://www.linkedin.com/in/maharshipatel1997/',
+        'Img': "static/Maharshi.jpeg",
+    },
+    {
+        'Name': 'Malcolm MacEachern',
+        'Bio': 'Malcolm is a graduating Electrical Engineer who was the main structural lead for this project, as well as assisting with aspects of dispension and locomotion sub-systems. He is looking forward to working on projects related to the fields of robotics & automation.',
+        'Task1': 'Structural Design/Simulation',
+        'Task2': 'Robot Assembly',
+        'Task3': 'General Engineering',
+        'LinkedIn': 'https://www.linkedin.com/in/malcolm-maceachern-6b2624116/',
+        'Img': "static/Malcolm.jpeg",
+    },
+    {
+        'Name': 'Joe Perri',
+        'Bio': 'Joe is an aspiring embedded systems engineer who worked on the firmware for the project, helping with the HMI and overall logic system. This project acted as a great opportunity to expand his experience in software design and image processing.',
+        'Task1': 'Navigation',
+        'Task2': 'Supervisory Logic',
+        'Task3': 'Multithreading',
+        'Git': 'https://github.com/joeperri95/',
+        'Img': "static/Joe.png"
+    },
+    {
+        'Name': 'Paul Nguyen',
+        'Bio': 'Paul is a B. Eng candidate at McMaster University. His technical contribution on the team focuses on the locomotion system. As a result of his involvement on the project, he wishes to pursue advance studies in motor design, inverter design and motor controls.',
+        'Task1': 'Locomotion',
+        'Task2': 'Power Systems',
+        'Task3': 'General Engineering',
+        'Img': "static/Random.jpeg"
+    }
+]
 
 if __name__ == '__main__':
 

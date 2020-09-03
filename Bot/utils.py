49f70f616bfbf1ca30e8fe1f2d9d0bab8b4da4d0
Joe Perri <joeperri95@gmail.com>
2020-09-03 15:31:58 -0400

    add basic test

--- Bot/utils.py
+++ Bot/utils.py
@@ -3,8 +3,15 @@
 
 import os
 
+
 def espeak(string):
     os.popen('espeak "' + string + '"')
 
 def kindReminder():
     os.popen('espeak "hey buddy I dont have all day"')
+    
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 5

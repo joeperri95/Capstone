3d72e5e27867f37d4992c6c217e774c65c7f6d7d
Joe Perri <joeperri95@gmail.com>
2020-09-03 15:59:01 -0400

    Update test_basic.py

--- test_basic.py
+++ test_basic.py
@@ -2,9 +2,12 @@
 import pytest
 
 @pytest.fixture
-def client(app):
-  clientApp =  app.test_client()
-    
+def clientApp(app):
+  client =  app.test_client()
+  return client
+  
+ 
+def test_about(clientApp):
   aboutResponse = clientApp.get('/about')
   aboutResponseLines = aboutResponse.split('\n')
   assert aboutResponseLines[0] == '<!DOCTYPE html>'
